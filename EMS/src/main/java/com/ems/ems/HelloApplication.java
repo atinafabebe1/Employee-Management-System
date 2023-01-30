8bce4217d60d5a41d89873cf8db9ffae90b1d42a
Atnaf <atinafabebe1@gmail.com>
2023-01-30 08:26:23 +0300

    authenticationfunctionality

--- EMS/src/main/java/com/ems/ems/HelloApplication.java
+++ EMS/src/main/java/com/ems/ems/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("EmployeeRecord.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("SignUp.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 560);
         stage.setTitle("EMS");
         stage.setScene(scene);
---
+++ EMS/src/main/java/com/ems/ems/SignUpController.java
@@ -0,0 +1,166 @@
+package com.ems.ems;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class SignUpController implements Initializable {
+
+    @FXML
+    private TextField addressTextField;
+
+    @FXML
+    private Label emailTextField;
+
+    @FXML
+    private TextField firstNameTextField;
+
+    @FXML
+    private TextField homeNumberTextField;
+
+    @FXML
+    private TextField lastNameTextField;
+
+    @FXML
+    private PasswordField password1TextFiled;
+
+    @FXML
+    private PasswordField password2TextField;
+
+    @FXML
+    private TextField phoneNumberTextFIeld;
+
+    @FXML
+    private Button registerbutton;
+
+    @FXML
+    private TextField roleTextField;
+
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private Label lblErrors;
+    @FXML
+    void handleRegisterButton(ActionEvent event) {
+
+            if (event.getSource() == registerbutton) {
+
+                if (signUp().equals("Success")) {
+                    try {
+                        Node node = (Node) event.getSource();
+                        Stage stage = (Stage) node.getScene().getWindow();
+                        stage.close();
+                        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("Login.fxml")));
+                        stage.setScene(scene);
+                        stage.show();
+                    } catch (IOException ex) {
+                        System.err.println(ex.getMessage());
+                    }
+
+                }
+            }
+        
+    }
+
+
+    Connection con = null;
+    PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
+
+    @FXML
+    
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        if (con == null) {
+            lblErrors.setTextFill(Color.TOMATO);
+            lblErrors.setText("Server Error : Check");
+        }
+    }
+
+    public SignUpController() {
+        con = ConnectionUtil.conDB();
+    }
+
+    private String signUp() {
+
+
+        String status = "Success";
+        String username = usernameTextField.getText();
+        String password1 = password1TextFiled.getText();
+        String password2 = password2TextField.getText();
+        String firstName = firstNameTextField.getText();
+        String lastName = lastNameTextField.getText();
+        String role = roleTextField.getText();
+        String email=emailTextField.getText();
+        String phoneNumber=phoneNumberTextFIeld.getText();
+        String homeNumber=homeNumberTextField.getText();
+        String address=addressTextField.getText();
+
+
+
+        if(username.isEmpty() || password1.isEmpty()
+                || password2.isEmpty()||firstName.isEmpty()
+                ||lastName.isEmpty()||role.isEmpty()
+                ||email.isEmpty()||phoneNumber.isEmpty()
+                ||homeNumber.isEmpty() ||address.isEmpty()
+        ) {
+            setLblError(Color.TOMATO, "All Filleds Are Required");
+            status = "Error";
+        } else if (!password1.equals(password2)) {
+            setLblError(Color.TOMATO, "Password Mismatch");
+            status = "Error";
+        } else {
+            //query
+            String sql = "INSERT INTO users "+
+                    "(phoneNumber,firstName,lastName,email,username,address,homeNo,password,role)" +
+                    " VALUES (?,?,?,?,?,?,?,?,?)";
+            try {
+                preparedStatement = con.prepareStatement(sql);
+                preparedStatement.setString(1, phoneNumber);
+                preparedStatement.setString(2, firstName);
+                preparedStatement.setString(3, lastName);
+                preparedStatement.setString(4, email);
+                preparedStatement.setString(5, username);
+                preparedStatement.setString(6, address);
+                preparedStatement.setString(7, homeNumber);
+                preparedStatement.setString(8, password1);
+                preparedStatement.setString(9, role);
+                int result=preparedStatement.executeUpdate();
+                if (result!=1) {
+                    setLblError(Color.TOMATO, "Something Wrong");
+                    status = "Error";
+                } else {
+                    setLblError(Color.GREEN, "Register Successful");
+                }
+            } catch (SQLException ex) {
+                System.err.println(ex.getMessage());
+                status = "Exception";
+            }
+        }
+
+        return status;
+    }
+
+    private void setLblError(Color color, String text) {
+        lblErrors.setTextFill(color);
+        lblErrors.setText(text);
+    }
+}
